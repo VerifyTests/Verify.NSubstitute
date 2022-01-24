fe59456c101d9f55cfa4b0552ccc22891b5423a7
Simon Cropp <simon.cropp@gmail.com>
2022-01-24 13:14:59 +1100

    Update CallConverter.cs

--- src/Verify.NSubstitute/CallConverter.cs
+++ src/Verify.NSubstitute/CallConverter.cs
@@ -1,16 +1,15 @@
-﻿using Newtonsoft.Json;
-using NSubstitute.Core;
+﻿using NSubstitute.Core;
 
 class CallConverter :
     WriteOnlyJsonConverter<ICall>
 {
-    public override void Write(VerifyJsonWriter writer, ICall value, JsonSerializer serializer)
+    public override void Write(VerifyJsonWriter writer, ICall value)
     {
         var target = new
         {
             Method = value.GetMethodInfo(),
             Arguments = value.GetArguments(),
         };
-        serializer.Serialize(writer, target);
+        writer.Serialize(target);
     }
 }
